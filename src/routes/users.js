308605e265d8a5d5a3c8468dbf5caba132ceb776
HupanFabricio <fabrihupan@gmail.com>
2023-04-22 10:34:48 -0300

    Solucion de error en flash el registrarse

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require('../models/User');
+
+
 router.get('/users/signin', (req,res) => {
     res.render('./users/signin.hbs')
 });
@@ -13,16 +16,12 @@ router.post('/users/signup', async(req,res) => {
     const { name, email, password, confirm_password } = req.body;
     const errors = [];
 
-    const User = require('../models/User');
-
     if(!name.length) {
         errors.push({text: 'Add name' })
     }
-
     if(!email.length) {
         errors.push({text: 'Add email' })
     }
-
     if(!password.length) {
         errors.push({text: 'Add password' })
     } else if (password.length < 4) {
@@ -34,19 +33,17 @@ router.post('/users/signup', async(req,res) => {
         res.render('./users/signup.hbs', { errors, name, email, password, confirm_password });
     } else {
         const emailUser = await User.findOne({email: email});
-        if(emailUser) {
-            req.flash('error_msg', 'The email is already in use');
-            res.redirect('/users/signup');  
-        }
         const newUser = new User ({name, email, password});
-        newUser.password = await newUser.encryptPassword(password);
-        await newUser.save();
-        req.flash('success_msg', 'You are registered');
-        res.redirect('/users/signin');
+        if(emailUser) {
+            req.flash('error_msg', 'Registered email');
+            res.redirect('/users/signup');  
+        } else {
+            newUser.password = await newUser.encryptPassword(password);
+            await newUser.save();
+            req.flash('success_msg', 'successfully registered');
+            res.redirect('/users/signin');
+        }
     }
 });
-
-
-
 
 module.exports = router;
