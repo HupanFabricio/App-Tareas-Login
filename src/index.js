d647e9b9bc6a986e07511f392d40ad23db9eb29e
HupanFabricio <fabrihupan@gmail.com>
2023-04-26 10:09:17 -0300

    Config port sv

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ require('./datebase');
 require('./config/passport');
 
 //Settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('views',  path.join(__dirname, 'views'))
 
 const  hbs =  exphbs.create({
